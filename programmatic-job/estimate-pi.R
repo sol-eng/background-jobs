a6732bd6f35511749ce2d2577070c0c81b86ef64
James Blair <james.m.blair09@gmail.com>
2019-06-10 17:10:18 -0600

    Add programmatic job and launcher examples

---
+++ programmatic-job/estimate-pi.R
@@ -0,0 +1,17 @@
+# Use a monte carlo simulation to estimate pi
+n <- 1e6
+if (!dir.exists("output")) dir.create("output")
+results_file <- tempfile("pi-", "output", ".rds")
+
+results <- matrix(nrow = n, ncol = 2)
+
+for (i in 1:n) {
+  results[i,] <- runif(2)
+  # Save out results every 25 iterations
+  if (i %% 25 == 0) {
+    print(paste0("Saving results (i = ", i, ")"))
+    save_results <- results[apply(results, 1, function(x) !all(is.na(x))),]
+    print(paste0("Pi estimate: ", mean((save_results[,1]^2 + save_results[,2]^2) <= 1) * 4))
+    saveRDS(save_results, results_file)
+  }
+}
---
+++ programmatic-job/launcher-jobs.R
@@ -0,0 +1,54 @@
+# Setup ----
+library(tidyverse)
+
+# Utils ----
+estimate_pi <- function(x) {
+  mean(x[,1]^2 + x[,2]^2 <= 1) * 4
+}
+
+# Start 5 concurrent Launcher jobs
+n <- 3
+jobs <- replicate(n, rstudioapi::launcherSubmitR(script = here::here("programmatic-job", "estimate-pi.R"), 
+                                                 cluster = "Kubernetes", 
+                                                 container = rstudioapi::launcherContainer("rstudio/r-session-complete:centos7-1.2.1511-1")))
+
+# Monitor job status
+# List all Pi output files
+pi_files <- fs::dir_ls(here::here("programmatic-job", "output"), regexp = "*.rds$")
+
+# Read all Pi output files and calculate Pi estimates
+pi_results <- map_df(pi_files, ~{
+  read_rds(.x) %>% 
+    as_tibble(.name_repair = ~make.names(., unique = TRUE)) %>% 
+    rename("Y" = "X.1") %>% 
+    mutate(pi_estimate = cumsum((X^2 + Y^2) <= 1)/row_number() * 4, 
+           file = .x)
+})
+
+# Output number of rows in each output file
+pi_results %>% 
+  count(file)
+
+# Estimate Pi using the results in each output file
+pi_results %>% 
+  group_by(file) %>%
+  filter(row_number() == max(row_number()))
+
+pi_results %>% 
+  group_by(file) %>%
+  summarise(pi_estimate = mean((X^2 + Y^2) <= 1) * 4)
+
+# Visualize convergence in each file
+pi_results %>% 
+  group_by(file) %>% 
+  mutate(obs = row_number() * 25) %>% 
+  ggplot(aes(x = obs, y = pi_estimate, group = file)) +
+  geom_hline(yintercept = pi, col = "red") +
+  # geom_point(alpha = .4) +
+  geom_line(alpha = .4) +
+  # facet_wrap(~file) +
+  theme_minimal() +
+  scale_x_continuous(labels = scales::comma) +
+  labs(title = "Pi Estimate Convergance",
+       y = "Pi Estimate",
+       x = "Observation")
